4f269aab0fec1101fe7428502338010291dcaedb
Estiven Álvarez Ballestero <estivenalvarezb@gmail.com>
2020-11-16 15:37:47 -0600

    Update calculadora.cs

--- calculadora.cs
+++ calculadora.cs
@@ -1,19 +1,1 @@
-private double dividir(int numero1, int numero2)
-{
-  return numero1/numero2;
-}
-
-private double multiplicar(int numero1, int numero2)
-{
-  return numero1*numero2;
-}
-
-private int suma(int numero1, int numero2)
-{
-	return numero1+numero2;
-}
-
-private int resta (int numero1, int numero2){
-  return numero1 - numero2;
-}
 
